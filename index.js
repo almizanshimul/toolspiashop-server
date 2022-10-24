6b9db61a57bd8f4c5bc441be262050b4f1e51b0d
Al <almizan1040@gmail.com>
2022-10-24 16:01:55 +0600

    mongo connected

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 
 app.use(express.json());
@@ -12,6 +13,23 @@ const port = process.env.PORT || 3500;
 app.get("/", (req, res) => {
   res.send(`ToolsPiaShop server is running on port http://localhost:${port}`);
 });
+
+// mongo DB connect message 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hobaswv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+  try {
+    await client.connect();
+    // console.log("Database Connected");
+    const toolsCollection = client.db("ToolsPiaShop").collection("tools");
+   
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, (req, res) => {
   console.log("ToolsPiaShop Server is running on port", port);
